8748d41f21d4ae39d18158d435e55aba6a22c8df
Ingrid Tufani <ingrid.tufani@hotmail.com>
2024-10-05 13:11:46 -0600

    Ingrid's Commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,34 @@
 ﻿
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ExpressionCalculator
+{
+    // Represent a token in the expression (number, operator, parentheses)
+    public class Token
+    {
+        public string Type { get; private set; }
+        public string Value { get; private set; }
+
+        public Token(string type, string value)
+        {
+            Type = type;
+            Value = value;
+        }
+    }
+
+    // Class for parsing the expression
+    public class Parser
+    {
+        private string expression;
+        private int index;
+
+        public Parser(string expression)
+        {
+            this.expression = expression;
+            this.index = 0;
+        }
+
+
+    }
