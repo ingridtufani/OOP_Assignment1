93fb45a5dbedbc3d470d4d178ce1190c1d647b8a
leolima2806 <leolima2806@gmail.com>
2024-10-05 20:03:53 -0600

    created token and expressionevaluator

--- Program.cs
+++ Program.cs
@@ -30,5 +30,61 @@ namespace ExpressionCalculator
             this.index = 0;
         }
 
+        // Reads numbers from the expression
+        private Token ParseNumber()
+        {
+            int startIndex = index;
+            while (index < expression.Length && (char.IsDigit(expression[index]) || expression[index] == '.'))
+            {
+                index++;
+            }
+
+            string number = expression.Substring(startIndex, index - startIndex);
+            return new Token("number", number);
+        }
+    }
+
+    // This class is for evaluates the expression
+    public class ExpressionEvaluator
+    {
+        private List<Token> tokens;
+        private int currentIndex;
+
+        public ExpressionEvaluator(List<Token> tokens)
+        {
+            this.tokens = tokens;
+            currentIndex = 0;
+        }
+
+        // Evaluates the expression
+        public double Evaluate()
+        {
+            return ParseExpression();
+        }
+
+        // Handles the precedence of operations (addition and subtraction)
+        private double ParseExpression()
+        {
+            double result = ParseTerm();
+
+            while (currentIndex < tokens.Count && (tokens[currentIndex].Value == "+" || tokens[currentIndex].Value == "-"))
+            {
+                string op = tokens[currentIndex].Value;
+                currentIndex++;
+                double right = ParseTerm();
+
+                if (op == "+")
+                {
+                    result += right;
+                }
+                else
+                {
+                    result -= right;
+                }
+            }
+
+            return result;
+        }
 
     }
+
